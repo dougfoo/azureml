9a9a9c8403893617636fdbcb164a0e1ac81974cd
dougfoo <doug.cha@gmail.com>
2019-10-14 00:35:21 -0400

    cleanup old files

--- infconfig.py
+++
@@ -1,6 +0,0 @@
-from azureml.core.model import InferenceConfig
-
-inference_config = InferenceConfig(runtime="python",
-                                   entry_script="score.py",
-                                   conda_file="myenv.yml")
-
